05581ace92d016288bb5ffc71d143050469505a8
Simona Mihai <19simona.mihai@gmail.com>
2019-11-11 21:08:37 +0200

    Add functionality to add expense

--- app/src/main/java/ro/simonamihai/a2lei/ExpenseActivity.java
+++ app/src/main/java/ro/simonamihai/a2lei/ExpenseActivity.java
@@ -1,13 +1,22 @@
 package ro.simonamihai.a2lei;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import org.w3c.dom.Text;
+
+import java.util.Date;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import ro.simonamihai.a2lei.db.DatabaseManager;
+import ro.simonamihai.a2lei.model.Expense;
 
 public class ExpenseActivity extends AppCompatActivity {
 
@@ -18,15 +27,27 @@ public class ExpenseActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Button btnInsertExpense = findViewById(R.id.insertExpenseBtn);
+        btnInsertExpense.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                TextView expenseName = findViewById(R.id.insertExpenseName);
+                TextView expensePrice = findViewById(R.id.inserExpensePrice);
+
+                Expense expense = new Expense(new Date(), expenseName.getText().toString(), Double.parseDouble(expensePrice.getText().toString()));
+
+                DatabaseManager databaseManager = new DatabaseManager(getApplicationContext());
+                databaseManager.open();
+                databaseManager.insert(expense);
+                databaseManager.close();
+                Snackbar.make(view, "Inserted", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
             }
         });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
 }
